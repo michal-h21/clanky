c3a2e080c654e99165d02a769d2b23cdbf716db2
Michal Hoftich <michal.h21@gmail.com>
2018-09-06 19:15:32 +0200

    Added css extract support

--- context_meeting_2018/xml/getcleanhtml.py
+++ context_meeting_2018/xml/getcleanhtml.py
@@ -1,7 +1,71 @@
+import argparse
+import logging
+import re
+from urllib.parse import urljoin
+from bs4 import BeautifulSoup
+import cssutils
 import requests
 from readability import Document
 
-response = requests.get('https://www.ctrl.blog/entry/browser-reading-mode-parsers')
-doc = Document(response.text)
+# process CSS files for @import directives
+# full css parsing is too slow
+class Imports():
+    def replace_imports(self, match):
+        replacement = ""
+        url = match.group(1)
+        if url:
+            newurl = urljoin(self.url, url)
+            csstext = requests.get(newurl)
+            if csstext:
+                newcss = Imports()
+                return newcss.process_css(csstext, newurl)
+        return replacement
+    # process css text for @import
+    def process_css(self, csstext, url):
+        self.url = url
+        if csstext:
+            regex = r"@import.*?[\"']([^\"']+)[\"'].*?;"
+            return re.sub(regex, self.replace_imports, csstext) 
+        return ""
+
+# don't show css parsing errors and warnings
+cssutils.log.setLevel(logging.CRITICAL)
+# process all CSS links, style elements and imports and create a one big CSS string
+def get_css(dom, baseurl):
+    cssutils.ser.prefs.keepComments = False
+    cssutils.ser.prefs.lineSeparator = " "
+    cssutils.ser.prefs.indent = ' ' 
+    csschunks = [] 
+    # links should be processed first
+    for link in dom.select('link[rel="stylesheet"]'):
+        src = link.get("href")
+        linkurl = urljoin(baseurl, src)
+        print("Download",linkurl)
+        csstext = requests.get(linkurl)
+        # style = cssutils.parseUrl(linkurl, encoding="utf-8", validate=False)
+        print("Parsed, resolve imports")
+        newcss = Imports().process_css(csstext.text, linkurl)
+        # style = cssutils.resolveImports(style)
+        print("Append it")
+        csschunks.append(newcss)
+        print("processed")
+        # print(style.cssText)
+    print("".join(csschunks))
+
+
+url = 'https://knihovna.pedf.cuni.cz/'
+
+
+soup = BeautifulSoup(requests.get(url).content,features="lxml")
+
+get_css(soup, url)
+
+# css = cssutils.parseUrl(url)
+
+# print(css.cssText)
+
+
+# response = requests.get(url)
+doc = Document(str(soup))
 print(doc.title())
 print(doc.summary())
