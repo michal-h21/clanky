c0e6c4473059311ef7790c45428d9528b706ad19
Michal Hoftich <michal.h21@gmail.com>
2018-09-07 22:52:19 +0200

    Added cache support

--- context_meeting_2018/xml/getcleanhtml.py
+++ context_meeting_2018/xml/getcleanhtml.py
@@ -1,11 +1,29 @@
-import argparse
-import logging
+import sys
+# import argparse
+# import logging
 import re
 from urllib.parse import urljoin
 from bs4 import BeautifulSoup
-import cssutils
 import requests
 from readability import Document
+import hashlib
+from diskcache import Cache
+
+class UrlCache:
+    def __init__(self, path):
+        self.path = path
+    # get url from cache or url
+    def get(self,url):
+        byteurl = bytes(url, 'utf-8')
+        byteurl = hashlib.sha224(byteurl).hexdigest()
+        with Cache(self.path) as cache:
+            if byteurl in cache:
+                return cache[byteurl]
+            else:
+                value = requests.get(url)
+                cache.add(byteurl, value)
+                return value
+
 
 # process CSS files for @import directives
 # full css parsing is too slow
@@ -14,8 +32,10 @@ class Imports():
         replacement = ""
         url = match.group(1)
         if url:
+            print("Import CSS", url)
             newurl = urljoin(self.url, url)
-            csstext = requests.get(newurl)
+            csstext = UrlCache("data").get(newurl)
+            # csstext = requests.get(newurl)
             if csstext:
                 newcss = Imports()
                 return newcss.process_css(csstext, newurl)
@@ -23,42 +43,96 @@ class Imports():
     # process css text for @import
     def process_css(self, csstext, url):
         self.url = url
-        if csstext:
+        if csstext and isinstance(csstext, str):
             regex = r"@import.*?[\"']([^\"']+)[\"'].*?;"
-            return re.sub(regex, self.replace_imports, csstext) 
+            try:
+                return re.sub(regex, self.replace_imports, csstext) 
+            except:
+                print(csstext)
+
         return ""
 
-# don't show css parsing errors and warnings
-cssutils.log.setLevel(logging.CRITICAL)
-# process all CSS links, style elements and imports and create a one big CSS string
-def get_css(dom, baseurl):
-    cssutils.ser.prefs.keepComments = False
-    cssutils.ser.prefs.lineSeparator = " "
-    cssutils.ser.prefs.indent = ' ' 
-    csschunks = [] 
-    # links should be processed first
-    for link in dom.select('link[rel="stylesheet"]'):
-        src = link.get("href")
-        linkurl = urljoin(baseurl, src)
-        print("Download",linkurl)
-        csstext = requests.get(linkurl)
-        # style = cssutils.parseUrl(linkurl, encoding="utf-8", validate=False)
-        print("Parsed, resolve imports")
-        newcss = Imports().process_css(csstext.text, linkurl)
-        # style = cssutils.resolveImports(style)
-        print("Append it")
-        csschunks.append(newcss)
-        print("processed")
-        # print(style.cssText)
-    print("".join(csschunks))
+class CleanHtml:
+    def __init__(self, url):
+        self.url = url
+        print("Download ", url)
+        page_content = requests.get(url).content
+        print("Parse DOM")
+        self.dom = BeautifulSoup(page_content,features="lxml")
+        print("Parse CSS")
+        self.css = self.get_css(self.dom, self.url)
+        # use full urls for images
+        print("Resolve images")
+        self.resolve_images()
+        print("Remove javascript")
+        self.remove_javascript()
+        self.template = """
+        <!DOCTYPE html>
+        <html>
+        <head>
+        <meta charset="utf-8" />
+        <title>{}</title>
+        <style type="text/css">
+        <![CDATA[
+        {}
+        ]]>
+        </style>
+        </head>
+        <body>
+        {}
+        </body>
+        </html>
+        """
+    # process all CSS links, style elements and imports and create a one big CSS string
+    def get_css(self,dom, baseurl):
+        csschunks = [] 
+        # links should be processed first
+        for link in dom.select('link[rel="stylesheet"]'):
+            src = link.get("href")
+            linkurl = urljoin(baseurl, src)
+            print("Process CSS", linkurl)
+            # csstext = requests.get(linkurl)
+            csstext = UrlCache("data").get(linkurl)
+            # style = cssutils.parseUrl(linkurl, encoding="utf-8", validate=False)
+            newcss = Imports().process_css(csstext.text, linkurl)
+            # style = cssutils.resolveImports(style)
+            csschunks.append(newcss)
+            # print(style.cssText)
+        for style in dom.select('style[type="text/css"]'):
+            print("Process CSS chunk")
+            csschunks.append(str(style))
+            # remove the style elements so they don't interfere with luaxml
+            style.decompose()
+        return "".join(csschunks)
+    def remove_javascript(self):
+        # we don't support scripts
+        for script in self.dom.select("script"):
+            script.decompose()
+    def resolve_images(self):
+        for img in self.dom.select("img"):
+            img.src = urljoin(self.url, img.get("src"))
+            # url = img.get("src")
+            # img.set(src=urljoin(self.url, url)
+    # hash images url for caching purposes
+    def hash_images(self):
+        if self.image_hashes:
+            for img in self.dom.select("img"):
+                print(img)
+    def get_clean(self):
+        doc = Document(self.dom.prettify(formatter="xml"))
+        return self.template.format(doc.title(), self.css, doc.summary(html_partial=True))
 
 
-url = 'https://knihovna.pedf.cuni.cz/'
 
 
-soup = BeautifulSoup(requests.get(url).content,features="lxml")
+url = sys.argv[1]
+clean = CleanHtml(url)
+# clean.hash_images()
+# print(clean.get_clean())
 
-get_css(soup, url)
+# soup = BeautifulSoup(requests.get(url).content,features="lxml")
+
+# get_css(soup, url)
 
 # css = cssutils.parseUrl(url)
 
@@ -66,6 +140,3 @@ get_css(soup, url)
 
 
 # response = requests.get(url)
-doc = Document(str(soup))
-print(doc.title())
-print(doc.summary())
